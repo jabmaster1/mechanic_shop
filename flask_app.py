489809cad44518591f9ed8cd04451344431e8fdb
Jobadiah Bryant <jabmaster112@gmail.com>
2026-03-07 21:20:02 -0500

    added a fix to allow url/api/docs to work and check on endpoints

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import os
 from application import create_app
 from application.models import db
 
@@ -6,3 +7,7 @@ app = create_app('ProductionConfig')
 with app.app_context():
     # db.drop_all()
     db.create_all()
+    
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
