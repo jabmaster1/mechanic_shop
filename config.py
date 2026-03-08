903bc6e24891fe350ff108d74f85d80ed0ceb2fb
Jobadiah Bryant <jabmaster112@gmail.com>
2026-03-07 21:02:36 -0500

    fixed path for render to deploy the app.

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class TestingConfig:
     CHACHE_TYPE = 'SimpleCache'
 
 class ProductionConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://jobadiahbryant:yxc4W3OAAUCSJ2rfrjxofHMZLSoydV4N@dpg-d6mcftrh46gs73bj1hr0-a.ohio-postgres.render.com/mechanic_shop_7out')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     CACHE_TYPE = 'SimpleCache'
 
